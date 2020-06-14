f2621fc6b1346d5f411815a1d20b623ac1ddf684
Razvan <brb_end1@yahoo.com>
2020-06-14 23:22:43 +0200

    solved opposite-day

--- scripts/1-opposite-day.js
+++ scripts/1-opposite-day.js
@@ -7,19 +7,21 @@ function oppositeDayHandler() {
   debugger;
 
   // read inputs from user
-  const showTheNumber = _(`do you want to see a random number?`);
+  const showTheNumber = alert(`do you want to see a random number?`);
 
   // before moving on ... type check!
-  if (typeof showTheNumber !== 'boolean') { throw new TypeError(); }
+  if (typeof showTheNumber !== "boolean") {
+    throw new TypeError();
+  }
 
   // perform core logic
   //  if they said they want to see the numbe, don't show it
   //  if they do want to see the number, do show it
-  const oppositeDay = _; // use a logical operator to reverse the value of their input
-  const message = _ ? (100 * Math.random()) : 'as you wish.  good day!';
+  const oppositeDay = showTheNumber; // use a logical operator to reverse the value of their input
+  const message = oppositeDay ? (100 * Math.random()) : 'as you wish.  good day!';
 
   // alert result for the user
-  alert(_);
+  alert(message);
 
   // log action for the developer
   console.log('\n--- opposite day ---');
